0ec6a4d1bd6d6673e5a6ec9bedbe76378ac306d8
Ao65 <ryu17615@gmail.com>
2025-08-30 01:07:37 +0900

    add stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,34 @@
+/** @type {import('stylelint').Config} */
+export default {
+  extends: ["stylelint-config-standard", "stylelint-config-html/astro"],
+  // ignoreFiles: ["./src/styles/reset.css"],
+  plugins: [
+    "stylelint-plugin-logical-css",
+    "stylelint-plugin-use-baseline",
+    "stylelint-declaration-block-no-ignored-properties",
+    "stylelint-use-nesting",
+    "stylelint-plugin-defensive-css",
+  ],
+  rules: {
+    "csstools/use-nesting": true,
+    "plugin/declaration-block-no-ignored-properties": true,
+    "plugin/use-baseline": [
+      true,
+      {
+        available: 2024,
+      },
+    ],
+    "plugin/use-defensive-css": [
+      true,
+      {
+        "accidental-hover": true,
+        "flex-wrapping": true,
+        "scroll-chaining": true,
+        "scrollbar-gutter": true,
+        "vendor-prefix-grouping": true,
+      },
+    ],
+    "plugin/use-logical-properties-and-values": [true, { severity: "warning" }],
+    "plugin/use-logical-units": [true, { severity: "warning" }],
+  },
+};
